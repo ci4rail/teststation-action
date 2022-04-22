928f580a2d71c3b99d1d6e91a9ad0329ef3f255f
popples <klauspopp@gmx.de>
2022-04-22 05:24:38 +0000

    artifact_override

--- generate-request.py
+++ generate-request.py
@@ -7,17 +7,18 @@ if len(sys.argv) != 4:
     print(f"Usage: {sys.argv[0]} pipeline test_name artifact_override")
     sys.exit(1)
 
-pipeline,test_name,artifact_override = sys.argv[1:4]
+pipeline,test_name,artifact_override_json = sys.argv[1:4]
 
-print("artifact_override='%s", artifact_override)
+artifact_override=json.loads(artifact_override_json)
+
+
+print("artifact_override=<<<%s>>>  <<<%s>>>" % (artifact_override_json, artifact_override))
 
 req = {
     "type": "Run Test",
     "user": pipeline,
     "test": test_name,
-    "desired_version_override": {
-        artifact_override
-    }
+    "desired_version_override": artifact_override
 }
 
 print(json.dumps(req))
